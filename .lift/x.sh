55aee40e7d2435c7593be5e2e1c742ddcc3ce798
gvazquezmorean <gvazquez@morean.co>
2022-11-02 10:45:49 -0300

    Update x.sh

--- .lift/x.sh
+++ .lift/x.sh
@@ -12,7 +12,6 @@ function tellApplicable() {
     printf "true\n" "7do par"
     printf "true\n" "8do par"
     printf "true\n" "9do par"
-    printf "true\n" "10do par"
 }
 
 function tellVersion() {
